36430f1d9e25db2ea049b4be498c2d219f61f007
Milad-97 <milad.aboueid.md@gmail.com>
2026-02-24 10:39:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import os
 
 # import os and use it to get the Github repository secrets
 MY_EMAIL = "general1497.j@gmail.com"
-MY_PASSWORD = "fhcj hudb gwbt llzl"
+MY_PASSWORD = ""
 today = datetime.now()
 today_tuple = (today.month, today.day)
 
